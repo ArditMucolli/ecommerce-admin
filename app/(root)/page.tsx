72da70268cbf8291a0f1242ce732a8871131a232
ArditMucolli <mucolliardit@gmail.com>
2023-10-25 19:29:25 +0200

    Install Clerk Authentication into my project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
-    <div>
-      <p>Hello Admin Dashboard</p>
-      <Button>Hello</Button>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
-}
+};
+
+export default SetupPage;
